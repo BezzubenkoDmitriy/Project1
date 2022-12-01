944b4a0867b6f98b1c3f3e2c69d27e16c117f078
Dmitriy <bezzubenko.dim@yandex.ru>
2022-12-01 13:12:02 +0700

    If_Else

---
+++ Example005_ifEls/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Урааа, это же Маша");
+}
+else
+{
+    Console.Write ("Привет,");
+    Console.Write (username);
+}
